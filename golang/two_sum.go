f0bdb62f7cb059a779c2d2d0496e36baa921f34f
jian-Lim <limjian1990@gmail.com>
2021-12-06 00:06:31 +0900

    two_sum.go

---
+++ golang/two_sum.go
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/two-sum/
+
+package main
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+
+	twoSum(nums, target)
+}
+
+func twoSum(nums []int, target int) []int {
+	var m map[int]int
+	m = map[int]int{}
+	// m := make(map[int]int)
+	// fmt.Println(m)
+	for i := 0; i < len(nums); i++ {
+		one := target - nums[i]
+		if _, ok := m[one]; ok {
+			return []int{m[one], i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
