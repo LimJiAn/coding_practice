52981c07f103fbed66cd4dcaf36f27a1d8430557
jian-Lim <limjian1990@gmail.com>
2021-12-06 00:11:05 +0900

    two_sum comment out

--- golang/two_sum.go
+++ golang/two_sum.go
@@ -5,15 +5,12 @@ package main
 func main() {
 	nums := []int{2, 7, 11, 15}
 	target := 9
-
 	twoSum(nums, target)
 }
 
 func twoSum(nums []int, target int) []int {
 	var m map[int]int
 	m = map[int]int{}
-	// m := make(map[int]int)
-	// fmt.Println(m)
 	for i := 0; i < len(nums); i++ {
 		one := target - nums[i]
 		if _, ok := m[one]; ok {
